451aa6fefe6551d859362e12f16f8c021745d0ec
David Cruz <david@codebycruz.com>
2026-03-11 17:47:01 -0700

    docs: add cursor locking functionality to example

--- examples/keyboard/src/init.lua
+++ examples/keyboard/src/init.lua
@@ -2,9 +2,9 @@ local winit = require("winit")
 
 local eventLoop = winit.EventLoop.new()
 local window = winit.Window.fromEventLoop(eventLoop)
-window:setTitle("keyboard demo")
 
 local heldKeys = {}
+local grabMode = "none" ---@type winit.CursorGrab
 
 local function updateTitle()
 	local keys = {}
@@ -12,8 +12,16 @@ local function updateTitle()
 		keys[#keys + 1] = k
 	end
 	table.sort(keys)
-	local title = #keys > 0 and table.concat(keys, " + ") or "keyboard demo"
-	window:setTitle(title)
+	local keysStr = #keys > 0 and table.concat(keys, " + ") or "none"
+	window:setTitle("keys: " .. keysStr .. "  |  grab: " .. grabMode .. "  (C=contain  L=locked  N=none)")
+end
+
+updateTitle()
+
+local function setGrab(mode)
+	grabMode = mode
+	window:setCursorGrab(mode)
+	updateTitle()
 end
 
 eventLoop:run(function(event, handler)
@@ -23,8 +31,17 @@ eventLoop:run(function(event, handler)
 		heldKeys = {}
 		updateTitle()
 	elseif event.name == "keyPress" then
-		heldKeys[event.key] = true
-		updateTitle()
+		local key = event.key
+		if key == "c" then
+			setGrab(grabMode == "contain" and "none" or "contain")
+		elseif key == "l" then
+			setGrab(grabMode == "locked" and "none" or "locked")
+		elseif key == "n" or key == "escape" then
+			setGrab("none")
+		else
+			heldKeys[key] = true
+			updateTitle()
+		end
 	elseif event.name == "keyRelease" then
 		heldKeys[event.key] = nil
 		updateTitle()
